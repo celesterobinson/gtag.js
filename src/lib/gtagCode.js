1bfc5bdc57c7353b8c901cd5922f69f7d3c57bc7
Celeste Robinson <crobinso@adobe.com>
2018-08-21 17:57:41 -0600

    added window.gtag to loadGtag function

--- src/lib/gtagCode.js
+++ src/lib/gtagCode.js
@@ -7,16 +7,14 @@ function loadGtag(conversionId) {
     if(!gtagLoaded) {
         var url = 'https://www.googletagmanager.com/gtag/js?id=' +  conversionId;
         loadScript(url);
-        window.dataLayer = window.dataLayer || [];
+        window.dataLayer = window.dataLayer || [];;
+        window.gtag = function(args) {
+            dataLayer.push(args);
+        };
         gtag(arguments);
         gtag('js', new Date());
         gtagLoaded = true;
     }
 }
-
-function gtag(layer){
-    dataLayer.push(layer);
-}
-
 
 module.exports = loadGtag;
