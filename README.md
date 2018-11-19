999ff07ca0dde0cdc7c0514e000193c7e5ec502c
Celeste Robinson <crobinso@adobe.com>
2018-11-19 11:05:37 -0700

    Added user input for renaming dataLayer and reconfigured init, getSettings, and validate functions

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Google gtag.js extension for Launch, by Adobe
+### Google gtag.js extension for Launch, by Adobe
 
 Load Google's gtag.js into your site to send event data to Google Analytics, Google Ads, and Google Marketing Platform.
--- src/lib/gtagCode.js
+++ src/lib/gtagCode.js
@@ -2,9 +2,16 @@
 
 var loadScript = require('@adobe/reactor-load-script');
 var conversionIdList = turbine.getExtensionSettings().conversionIdList;
+// var dataLayer = turbine.getExtensionSettings().renamedDataLayer || 'dataLayer';
 
 var url = 'https://www.googletagmanager.com/gtag/js?id=' + conversionIdList[0];
 loadScript(url);
+
+if (window.dataLayer && !Array.isArray(window.dataLayer)) {
+    console.log('window.dataLayer already exists and is not an array. Visit your extension configuration in Launch, and' +
+        'reconfigure the dataLayer variable to something else of your choosing.')
+}
+
 window.dataLayer = window.dataLayer || [];
 window.gtag = function () {
     dataLayer.push(arguments);
